884833a1b21d447c4274934a95828edd9af21bd5
Louis-Philippe <louis.philippe.heon@gmail.com>
2024-03-04 23:10:57 -0500

    Config

--- config.ts
+++ config.ts
@@ -1,10 +1,14 @@
-import { Rcon } from 'rcon-client';
+import { RconClient } from 'rconify';
 
 export const token: string = 'aHR0cHM6Ly9pLmt5bS1jZG4uY29tL3Bob3Rvcy9pbWFnZXMvb3JpZ2luYWwvMDAxLzY5My8zOTUvOTc0LnBuZw==';
 export const clientId: string = 'YWRlcHRDb250ZW50LnppcA==';
 export const guildId: string = 'dGEgbcOocmUgZXN0IGdyb3NzZQ==';
 
-export const rconConnexions: Rcon[] = [
-	new Rcon({ host: '10.0.0.100', port: 420, password: 'U0VSSVNFVA==' }),
-	new Rcon({ host: '10.0.0.100', port: 69, password: 'U0VSSVNFVA==' })
+export const minecraftServerName = 'SpiceCraft';
+export const playerRoleName = 'Joueur';
+export const whitelistChannelName = 'whitelist';
+
+export const rconConnexions: RconClient[] = [
+	new RconClient({ host: '10.0.0.100', port: 420, password: 'U0VSSVNFVA==', ignoreInvalidAuthResponse: false }),
+	new RconClient({ host: '10.0.0.100', port: 69, password: 'U0VSSVNFVA==', ignoreInvalidAuthResponse: false })
 ]
